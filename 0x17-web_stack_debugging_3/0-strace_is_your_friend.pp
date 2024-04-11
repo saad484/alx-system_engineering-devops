5930d0ff5c8df09c2b18c8314543289be01783df
saad484 <aiddisaad484@gmail.com>
2024-04-11 16:29:52 +0000

    strace

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,7 @@
+# Fixes bad `phpp` extensions to `php` in the wordPress file `wp-settings.php`.
+
+exec { 'fix-wordpress':
+  command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+  path    => '/usr/local/bin/:/bin/'
+}
+
