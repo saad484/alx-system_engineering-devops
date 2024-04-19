c16cf060edc22ced06b82f2c9b487fb1511dce19
saad484 <aiddisaad484@gmail.com>
2024-04-19 22:31:05 +0100

    user limit

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,4 @@
-# Reconfigure the OS for 'holberton' to login and open a file without any error message
+# Reconfigure the OS for 'holberton' to login and open a file without any error message.
 
 exec { 'increase-hard-file-limit-holberton-user':
   command => 'sed -i "/holberton hard/s/4/50000/" /etc/security/limits.conf',
